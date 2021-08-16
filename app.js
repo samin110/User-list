f3d2e99b2cfc76b4841fc9715d7f04c2511130c5
amin naseralddini <s.amin.naseralddini@gmail.com>
2021-08-16 12:15:11 +0430

    edite for responsive and use sass

--- app.js
+++ app.js
@@ -3,67 +3,74 @@ async function fetchApi() {
 		.then(response => response.json())
 		.then((list) => {
 			let html = '';
-			list.forEach(element => {
-				if (element.id >= 7) {
-				} else {
+			list.map(element => {
+				if (element.id >= 7) {} else {
 					html +=
 						`
-				<div class="table-card">
-						<table >
-							<thead>
-								<tr class="ta-head">
-									<th>Id</th>
-								</tr>
-								<tr class="ta-head">
-									<th>Name</th>
-								</tr>
-								<tr class="ta-head">
-									<th>Username</th>
-								</tr>
-								<tr class="ta-head">
-									<th>Email</th>
-								</tr>
-								<tr class="ta-head">
-									<th>Address</th>
-								</tr>
-								<tr class="ta-head">
-									<th>Phone</th>
-								</tr>
-								<tr class="ta-head">
-									<th>company</th>
-								</tr>
-								<tr class="ta-head">
-									<th>Web</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr class="ta-head">
-									<td style="background-color:#2ab82f; color:white; padding:6px; border-radius:30%;">${element.id}</td>
-								</tr>
-								<tr class="ta-head">
-									<td>${element.name}</td>
-								</tr>
-								<tr class="ta-head">
-									<td>${element.username}</td>
-								</tr>
-								<tr class="ta-head">
-									<td>${element.email}</td>
-								</tr>
-								<tr class="ta-head">
-									<td>${element.address.street}-${element.address.city}</td>
-								</tr>
-								<tr class="ta-head">
-									<td>${element.phone}</td>
-								</tr>
-								<tr class="ta-head">
-									<td>${element.company.name}</td>
-								</tr>
-								<tr class="ta-head">
-									<td><a href="https://www.${element.website}" style="color:#2ab82f; text-align:center;" ><i class="fas fa-link"></i></a></td>
-								</tr>
-							</tbody>
+						<div class="table-card">
+						<table>
+						  <thead>
+							<tr class="table-card__ta-head">
+							  <th>Id</th>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <th>Name</th>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <th>Username</th>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <th>Email</th>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <th>Address</th>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <th>Phone</th>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <th>company</th>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <th>Web</th>
+							</tr>
+						  </thead>
+						  <tbody>
+							<tr class="table-card__ta-head">
+							  <td class="table-card--id" >
+								${element.id}
+							  </td>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <td>${element.name}</td>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <td>${element.username}</td>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <td>${element.email}</td>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <td>${element.address.street}-${element.address.city}</td>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <td>${element.phone}</td>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <td>${element.company.name}</td>
+							</tr>
+							<tr class="table-card__ta-head">
+							  <td>
+								<a
+								  href="https://www.${element.website}"
+								  style="color: #2ab82f; text-align: center"
+								  ><i class="fas fa-link"></i
+								></a>
+							  </td>
+							</tr>
+						  </tbody>
 						</table>
-					</div>`;
+					  </div> `;
 				}
 
 			});
@@ -76,73 +83,81 @@ fetchApi();
 
 /** Event for click button Add User  */
 
-document.querySelector('.button i').addEventListener('click', addUser);
+document.querySelector('.btn i').addEventListener('click', addUser);
 
 async function addUser() {
 	await fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
 		.then((list) => {
 			let html = '';
-			list.forEach(element => {
+			list.map(element => {
 				if (element.id >= 7) {
 					html +=
 						`
-			<div class="table-card">
-					<table>
-						<thead>
-							<tr class="ta-head">
-								<th>Id</th>
+						<div class="table-card">
+						<table>
+						  <thead>
+							<tr class="table-card__ta-head">
+							  <th>Id</th>
 							</tr>
-							<tr class="ta-head">
-								<th>Name</th>
+							<tr class="table-card__ta-head">
+							  <th>Name</th>
 							</tr>
-							<tr class="ta-head">
-								<th>Username</th>
+							<tr class="table-card__ta-head">
+							  <th>Username</th>
 							</tr>
-							<tr class="ta-head">
-								<th>Email</th>
+							<tr class="table-card__ta-head">
+							  <th>Email</th>
 							</tr>
-							<tr class="ta-head">
-								<th>Address</th>
+							<tr class="table-card__ta-head">
+							  <th>Address</th>
 							</tr>
-							<tr class="ta-head">
-								<th>Phone</th>
+							<tr class="table-card__ta-head">
+							  <th>Phone</th>
 							</tr>
-							<tr class="ta-head">
-								<th>company</th>
+							<tr class="table-card__ta-head">
+							  <th>company</th>
 							</tr>
-							<tr class="ta-head">
-								<th>Web</th>
+							<tr class="table-card__ta-head">
+							  <th>Web</th>
 							</tr>
-						</thead>
-						<tbody>
-							<tr class="ta-head">
-								<td style="background-color:#2ab82f; color:white; padding:6px; border-radius:30%;">${element.id}</td>
+						  </thead>
+						  <tbody>
+							<tr class="table-card__ta-head">
+							  <td class="table-card--id" >
+								${element.id}
+							  </td>
 							</tr>
-							<tr class="ta-head">
-								<td>${element.name}</td>
+							<tr class="table-card__ta-head">
+							  <td>${element.name}</td>
 							</tr>
-							<tr class="ta-head">
-								<td>${element.username}</td>
+							<tr class="table-card__ta-head">
+							  <td>${element.username}</td>
 							</tr>
-							<tr class="ta-head">
-								<td>${element.email}</td>
+							<tr class="table-card__ta-head">
+							  <td>${element.email}</td>
 							</tr>
-							<tr class="ta-head">
-								<td>${element.address.street}-${element.address.city}</td>
+							<tr class="table-card__ta-head">
+							  <td>${element.address.street}-${element.address.city}</td>
 							</tr>
-							<tr class="ta-head">
-								<td>${element.phone}</td>
+							<tr class="table-card__ta-head">
+							  <td>${element.phone}</td>
 							</tr>
-							<tr class="ta-head">
-								<td>${element.company.name}</td>
+							<tr class="table-card__ta-head">
+							  <td>${element.company.name}</td>
 							</tr>
-							<tr class="ta-head">
-								<td><a href="https://www.${element.website}" style="color:#2ab82f; text-align:center;" ><i class="fas fa-link"></i></a></td>
+							<tr class="table-card__ta-head">
+							  <td>
+								<a
+								  href="https://www.${element.website}"
+								  style="color: #2ab82f; text-align: center"
+								  ><i class="fas fa-link"></i
+								></a>
+							  </td>
 							</tr>
-						</tbody>
-					</table>
-				</div>`;
+						  </tbody>
+						</table>
+					  </div>`;
 				}
 
 
